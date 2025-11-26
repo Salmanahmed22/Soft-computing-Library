30ffc1b72f7543d1871016d941c94a5464376962
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-11-26 19:46:21 +0200

    linguistic variables

---
+++ src/main/java/org/codeWithGA/FuzzyLogic/variables/LinguisticVariable.java
@@ -0,0 +1,37 @@
+package org.codeWithGA.FuzzyLogic.variables;
+
+import org.codeWithGA.FuzzyLogic.membership.MembershipFunction;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+this class is for each input variable in the fuzzy logic
+ */
+public class LinguisticVariable {
+    private String name;
+    private double minValue,maxValue;
+    private Map<String, MembershipFunction> fuzzySets;
+
+    public LinguisticVariable(String name, double minValue, double maxValue) {}
+
+    public void addFuzzySet(String label, MembershipFunction fuzzySet) {
+        fuzzySets.put(label, fuzzySet);
+    }
+
+    public Map<String, Double> fuzzify(double value) {
+        value = clamp(value);
+        double finalValue = value;
+        return fuzzySets.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey(),
+                        entry -> entry.getValue().evaluate(finalValue)
+                ));
+    }
+
+    // IF THE GIVEN VALUE IS OUT OF RANGE IT CLAMPS TO THE RANGE BOUNDARIES
+    private double clamp(double v) {
+        return Math.max(minValue, Math.min(maxValue, v));
+    }
+}
