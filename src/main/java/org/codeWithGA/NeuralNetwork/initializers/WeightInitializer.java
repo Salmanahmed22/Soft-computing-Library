5ffbc994e4da1693ea2e7fbf79ee0925fd64a3e2
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-12-19 20:25:24 +0200

    some comments

--- src/main/java/org/codeWithGA/NeuralNetwork/initializers/WeightInitializer.java
+++ src/main/java/org/codeWithGA/NeuralNetwork/initializers/WeightInitializer.java
@@ -1,5 +1,9 @@
 package org.codeWithGA.NeuralNetwork.initializers;
-
+//Why this layer is important?
+//Poor initialization → vanishing/exploding gradients
+//Good initialization → faster convergence
 public interface WeightInitializer {
+    //rows -> number of neurons in this layer
+    //cols -> number of inputs from the previous layer
     double[][] initialize(int rows, int cols);
 }
